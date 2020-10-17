69dda66cc9ccfbd238cd19f4316ae6a2b7c5d01e
unknown <argya.programmer@gmail.com>
2020-10-17 14:43:52 +0700

    Initial Commit

---
+++ src/com/tugasPBO/logika.java
@@ -0,0 +1,18 @@
+//latihan 15 PBO
+// Nama       : Argya Aulia Fauzandika
+// NIM        : 10119035
+// Kelas      : IF-1
+// Deskripsi  : menampilkan logika true atau false memakai
+//	            operator relasi and, or, not dan not and
+package com.tugasPBO;
+
+public class logika {
+    public static void main(String[] args){
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+}
